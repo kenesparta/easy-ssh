13a9e8881cadd441e384853bd0b06cd4b772e5cf
Ken Esparta Ccorahua <kenesparta@pm.me>
2020-07-04 19:19:24 -0500

    remove idea folder

--- backend/main.go
+++ backend/main.go
@@ -1,6 +1,23 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+)
 
+func check(e error) {
+	if nil != e {
+		log.Println(e)
+	}
 }
 
+func readFile(fileName string) {
+	dat, err := ioutil.ReadFile(fileName)
+	check(err)
+	fmt.Print(string(dat))
+}
+
+func main() {
+	readFile("config")
+}
